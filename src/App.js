4dd8c3d7fc0a24377d697eedc2b37a59a323fbee
Aaroh Sinha <121655529+AarohSinha100@users.noreply.github.com>
2023-06-22 21:36:41 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+import './App.css';
+import nikelogo from "C:\\Users\\PROGRAMMING\\Desktop\\PROJECT_1 (REACT)\\reactprojectone\\src\\images\\brand_logo.png"
+
+import NavBar from './components/NavBar';
+import Body from './components/Body';
+
+function App() {
+  return(
+    <div>
+       <NavBar />
+       <Body />
+    </div>
+   
+    
+  )
+  }
+
+export default App;
